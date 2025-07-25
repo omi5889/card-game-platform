485b85eed84262acd950bb9ac00efea25d82ed7b
Omkar Gadgil <omi5889@gmail.com>
2025-07-25 16:03:57 -0400

    use deployed backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,2 +1,3 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3001"); // Update later for production
+// export const socket = io("http://localhost:3001"); // Update later for production
+export const socket = io(import.meta.env.VITE_BACKEND_URL); // if using Vite
